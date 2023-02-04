5b55417135136269c809a74794d0d0a5e6a9113f
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-02-04 16:30:52 +0900

    fix pep8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -24,7 +24,6 @@ class Group(models.Model):
         super().save(*args, **kwargs)
 
 
-
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
